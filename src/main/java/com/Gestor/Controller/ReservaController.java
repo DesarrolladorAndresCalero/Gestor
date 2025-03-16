5f099fdf99b79c37701e8929503804dd95588dfa
Andres Felipe Calero Arana <111255268+DesarrolladorAndresCalero@users.noreply.github.com>
2025-03-16 12:06:54 -0500

    avances back

--- src/main/java/com/Gestor/Controller/ReservaController.java
+++ src/main/java/com/Gestor/Controller/ReservaController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/reservas")
 public class ReservaController {
--- src/main/java/com/Gestor/Controller/UsuarioController.java
+++ src/main/java/com/Gestor/Controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.Gestor.Controller;
 
 import com.Gestor.Model.Usuario;
+import com.Gestor.Repository.UsuarioRepository;
 import com.Gestor.Service.UsuarioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,13 +9,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
+
     private final UsuarioService usuarioService;
 
-    public UsuarioController(UsuarioService usuarioService) {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioController(UsuarioService usuarioService, UsuarioRepository usuarioRepository) {
         this.usuarioService = usuarioService;
+        this.usuarioRepository = usuarioRepository;
     }
 
     @PostMapping("/registrar")
@@ -33,5 +39,18 @@ public class UsuarioController {
     public ResponseEntity<List<Usuario>> listarUsuarios() {
         List<Usuario> usuarios = usuarioService.listarUsuarios();
         return ResponseEntity.ok(usuarios);
+    }
+
+    @PostMapping("/login")
+    public Usuario login(@RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioEncontrado = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if (usuarioEncontrado.isPresent()) {
+            Usuario usuarioDB = usuarioEncontrado.get();
+            if (usuarioDB.getContrasena().equals(usuario.getContrasena())) {
+                return usuarioDB; // Enviar usuario con su rol
+            }
+        }
+        return null; // Retornar null si el usuario no existe o la contraseña es incorrecta
     }
 }
